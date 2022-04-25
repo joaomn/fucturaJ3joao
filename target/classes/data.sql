b21457f3c1400fde03f220f424a84bb59e1342e0
joao araujo <joaoaraujomn@gmail.com>
2022-04-25 19:43:51 -0300

    teste dois

---
+++ target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into jogador (nome, idade, altura, peso, img, id) values ('messi', 32, 1.78, 90, '', 1);
+insert into jogador (nome, idade, altura, peso, img, id) values ('Benzema', 36, 1.88, 98, '', 2);
+insert into jogador (nome, idade, altura, peso, img, id) values ('Carlinhos Bala', 44, 1.68, 89, '', 3);
